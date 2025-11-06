5eb4bbb3fe39d4d2d975d68c636aa9c259c9744e
zyr4c31 <zyr4c31@gmail.com>
2025-11-06 15:13:45 +0800

    add datetime

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/starfederation/datastar-go/datastar"
 )
+
+type Store struct {
+	Date         string `json:"date"`
+	UTCOffsetMin int    `json:"tzoffset"`
+	TZ           string `json:"tz"`
+}
 
 func main() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +22,16 @@ func main() {
 		component.Render(r.Context(), w)
 	})
 
-	http.HandleFunc("GET /time", func(w http.ResponseWriter, r *http.Request) {
-		formatString := "January 02, 2006 03:04:05 PM"
-		datetime := time.Now().Local().Format(formatString)
-		change := Change(datetime)
+	http.HandleFunc("POST /time", func(w http.ResponseWriter, r *http.Request) {
+		store := &Store{}
+		_ = datastar.ReadSignals(r, store)
+		fmt.Printf("store.TZ: %v\n", store.TZ)
+		offsetSeconds := store.UTCOffsetMin * -60
+		userTimeZone := time.FixedZone("user", offsetSeconds)
+
+		t := time.Now().In(userTimeZone)
+		fmt.Printf("t: %v\n", t)
+		change := Change(t.Local().String())
 		sse := datastar.NewSSE(w, r)
 		sse.PatchElementTempl(change)
 	})
